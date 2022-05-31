e4d523f80937dc776d3beff2f5ec54a2aef7e7b7
darius owens <darius.owens.j@gmail.com>
2022-05-30 17:38:08 -0700

    added stack and queue functionallity

--- java/datastructures/lib/src/main/java/stack-queue/Node.java
+++ java/datastructures/lib/src/main/java/stack-queue/Node.java
@@ -1,1 +1,11 @@
 package datastructures.stack-queue;
+
+public class Node<T>{
+  T value;
+  Node<T> next = null;
+
+  public Node(T value) {
+    this.value = value;
+
+  }
+}
--- java/datastructures/lib/src/main/java/stack-queue/Stack.java
+++ java/datastructures/lib/src/main/java/stack-queue/Stack.java
@@ -1,1 +1,37 @@
 package datastructures.stack-queue;
+
+import java.util.NoSuchElementException;
+
+public class Stack {
+  Node<T> top;
+
+
+public void push(T value){
+  Node<T> node = new Node<T>(value);
+  if(top != null){
+    node.next = top;
+  }
+  top = node;
+}
+
+public T pop(){
+  if (this.isEmpty()){
+    throw new NoSuchElementException();
+  } else {
+  int data = top.data;
+  top = top.next;
+  return data;
+  }
+
+}
+  public int peek(){
+    if (this.isEmpty()){
+      throw new NoSuchElementException();
+    }
+    else return top.data;
+  }
+
+  public boolean isEmpty(){
+  return this.top == null;
+  }
+}
