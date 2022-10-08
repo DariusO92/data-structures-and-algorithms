3679b8020a690d3a63f9db52aaaa374dfd681329
Darius <Darius.owens.j@gmail.com>
2022-10-08 14:05:22 -0700

    added edge

--- java/datastructures/lib/src/main/codechallenges/datastructures/Graphs/Graphs.java
+++ java/datastructures/lib/src/main/codechallenges/datastructures/Graphs/Graphs.java
@@ -4,12 +4,15 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Graphs<T extends Comparable<? super T>> implements Comparable<Graphs<T>> {
-
+  private boolean isWeighted;
+  private boolean isDirected;
   public HashMap<Vertex<T>, LinkedList<Edge<T>>> adjacencyLists;
   private int numberOfVertices = 0;
 
-  public Graphs(int numberOfVertices){
+  public Graphs(int numberOfVertices, boolean inputIsWeighted, boolean inputIsDirected){
     this.numberOfVertices = numberOfVertices;
+    this.isWeighted = inputIsWeighted;
+    this.isDirected = inputIsWeighted;
   }
 
   public Vertex<T> addVertex(T value) {
@@ -23,6 +26,13 @@ public class Graphs<T extends Comparable<? super T>> implements Comparable<Graph
     addEdge(start, destination, 0);
   }
   public void addEdge(Vertex<T> start, Vertex<T> destination, int weight){
+    if (!this.isWeighted) {
+      weight = 0;
+    }
+    start.addEdge(destination, weight);
+    if (!this.isDirected){
+      destination.addEdge(start, weight);
+    }
 
   }
 
